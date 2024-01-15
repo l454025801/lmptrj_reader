f400ec7ecb8150d53f999be3426a8b34ca48c9f9
YANG LI <60527575+l454025801@users.noreply.github.com>
2024-01-15 11:38:57 -0600

    Update lmptrj_reader.py

--- lmptrj_reader.py
+++ lmptrj_reader.py
@@ -3,6 +3,10 @@ import numpy as np
 import pandas as pd
 
 def read_lmptrj(lmptrj, steps, frames, trj_format):
+    '''
+        lmptrj: lammps dump trajectory
+    trj_format: List. the format of the dump file. Ex: ["index", "type", "x", "y", "z"]
+    '''
     lmptrj_file = open(lmptrj,'r')
     
     # first store each frame as a key in a dictionary
